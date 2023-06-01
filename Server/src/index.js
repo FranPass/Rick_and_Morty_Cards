1368ea65455bead10f2cc0660ff9f31d5c6b83d9
FPassetti <franciscopassettimail.com>
2023-06-01 10:55:36 -0300

    mod_server

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,33 +1,9 @@
-const express = require('express');
-const server = express();
+const server = require('./server');
 const PORT = 3001;
-const morgan = require('morgan')
-const router = require('./routes/index');
 const { conn } = require('./DB_connection');
 
-conn.sync()
-
-server.use(express.json());
-server.use(morgan('dev'));
-
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header(
-       'Access-Control-Allow-Headers',
-       'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    res.header(
-        'Access-Control-Allow-Methods',
-        'GET, POST, OPTIONS, PUT, DELETE'
-    );
-    next();
- });
-
-
- server.use('/rickandmorty', router)
-
-
+conn.sync({force: true});
+      
 server.listen(PORT, () => {
    console.log('Server raised in port: ' + PORT);
 });
